2d90557611f3464814438b5b37cc1a5c39224f06
Glyn Matthews <glyn.matthews@gmail.com>
2020-04-13 11:05:39 +0200

    Added implementation and tests for converting a query string to a JSON data structure and back

--- include/skyr/json/json.hpp
+++ include/skyr/json/json.hpp
@@ -7,39 +7,73 @@
 #define SKYR_URL_JSON_HPP
 
 #include <string>
-#include <skyr/query/query_iterator.hpp>
+#include <system_error>
+#include <vector>
+#include <tl/expected.hpp>
 #include <nlohmann/json.hpp>
+#include <skyr/query/query_iterator.hpp>
+#include <skyr/percent_encoding/percent_encode.hpp>
+#include <skyr/percent_encoding/percent_decode.hpp>
 
 namespace skyr {
 inline namespace v1 {
 namespace json {
-struct decode_options {
+inline auto encode_query(const nlohmann::json &json, char separator='&', char equal='=')
+  -> tl::expected<std::string, std::error_code> {
+  using namespace std::string_literals;
 
-};
+  auto result = ""s;
+  for (auto &[key, value] : json.items()) {
 
-inline std::string encode(const nlohmann::json json, char separator = '&', char equal = '=') {
-  return "";
+    if (value.is_string()) {
+      result += percent_encode<std::string>(key);
+      result += equal;
+      result += percent_encode<std::string>(value.get<std::string>());
+      result += separator;
+    }
+    else if (value.is_array()) {
+      for (auto &element : value.items()) {
+        result += percent_encode<std::string>(key);
+        result += equal;
+        result += percent_encode<std::string>(element.value().get<std::string>());
+        result += separator;
+      }
+    }
+    else {
+      result += percent_encode<std::string>(key);
+      result += equal;
+      result += separator;
+    }
+  }
+
+  return result.substr(0, result.size() - 1);
 }
 
-nlohmann::json decode(std::string_view query, char separator = '&', char equal = '=') {
+inline auto decode_query(std::string_view query, char separator='&', char equal='=') -> nlohmann::json {
   if (query[0] == '?') {
-    query.remove_prefix(0);
+    query.remove_prefix(1);
   }
 
   nlohmann::json object;
   for (auto [key, value] : query_parameter_range(query)) {
-    if (object.end() == object.find(key)) {
-      object[key] = value;
+    const auto key_ = skyr::percent_decode<std::string>(key).value();
+    const auto value_ = skyr::percent_decode<std::string>(value).value();
+
+    if (object.contains(key_)) {
+      auto current_value = object[key_];
+      if (current_value.is_string()) {
+        auto prev_value = current_value.get<std::string>();
+        object[key_] = std::vector<std::string>{prev_value, value_};
+      }
+      else if (current_value.is_array()) {
+        auto values = current_value.get<std::vector<std::string>>();
+        values.emplace_back(value_);
+        object[key_] = values;
+      }
     }
     else {
-      if (object[key].is_object()) {
-        //  object[key] = { object.get(key), value };
-      }
-      else {
-        // object.arr
-      }
+      object[key_] = value_;
     }
-    object[key] = value;
   }
   return object;
 }
