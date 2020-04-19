cpp-netlib/url

commit e0c32ad9e7a1a6e5ef325e73075ca6874335bd2f

Refactor trailing return types (#59)
* Applied trailing return type to functions in core and details

* Applied trailing return type wherever it made sense

src/core/url_parser_context.cpp
@@ -24,14 +24,14 @@ using namespace std::string_literals;
 using namespace std::string_view_literals;
 
 namespace {
-inline bool contains(
+inline auto contains(
     char byte,
     std::string_view view) noexcept {
   auto first = begin(view), last = end(view);
   return last != std::find(first, last, byte);
 }
 
-bool remove_leading_whitespace(std::string &input) {
+auto remove_leading_whitespace(std::string &input) {
   auto view = std::string_view(input);
   auto first = begin(view), last = end(view);
   auto it = std::find_if(
@@ -45,7 +45,7 @@ bool remove_leading_whitespace(std::string &input) {
   return it == first;
 }
 
-bool remove_trailing_whitespace(std::string &input) {
+auto remove_trailing_whitespace(std::string &input) {
   using reverse_iterator =
   std::reverse_iterator<std::string::const_iterator>;
 
@@ -64,7 +64,7 @@ bool remove_trailing_whitespace(std::string &input) {
   return it == first;
 }
 
-bool remove_tabs_and_newlines(std::string &input) {
+auto remove_tabs_and_newlines(std::string &input) {
   auto first = begin(input), last = end(input);
   auto it = std::remove_if(
       first, last, [] (auto byte) -> bool { return contains(byte, "\t\r\n"sv); });
@@ -72,14 +72,14 @@ bool remove_tabs_and_newlines(std::string &input) {
   return it == last;
 }
 
-inline bool is_forbidden_host_point(std::string_view::value_type byte) noexcept {
+inline auto is_forbidden_host_point(std::string_view::value_type byte) noexcept {
   using namespace std::string_view_literals;
   static const auto forbidden = "\0\t\n\r #%/:?@[\\]"sv;
   auto first = begin(forbidden), last = end(forbidden);
   return last != std::find(first, last, byte);
 }
 
-bool remaining_starts_with(
+auto remaining_starts_with(
     std::string_view input,
     std::string_view chars) noexcept {
   if (input.empty()) {
@@ -88,7 +88,7 @@ bool remaining_starts_with(
   return starts_with(input.substr(1), chars);
 }
 
-tl::expected<std::string, url_parse_errc> parse_opaque_host(std::string_view input) {
+auto parse_opaque_host(std::string_view input) -> tl::expected<std::string, url_parse_errc> {
   auto first = begin(input), last = end(input);
   auto it = std::find_if(
       first, last, [] (auto byte) -> bool {
@@ -107,8 +107,8 @@ tl::expected<std::string, url_parse_errc> parse_opaque_host(std::string_view inp
   return output;
 }
 
-tl::expected<std::string, url_parse_errc> parse_host(
-    std::string_view input, bool is_not_special = false) {
+auto parse_host(
+    std::string_view input, bool is_not_special = false) -> tl::expected<std::string, url_parse_errc> {
   if (!input.empty() && (input.front() == '[')) {
     if (input.back() != ']') {
       // result.validation_error = true;
@@ -161,7 +161,7 @@ tl::expected<std::string, url_parse_errc> parse_host(
   return host.value().to_string();
 }
 
-tl::expected<std::uint16_t, url_parse_errc> port_number(std::string_view port) noexcept {
+auto port_number(std::string_view port) noexcept -> tl::expected<std::uint16_t, url_parse_errc> {
   if (port.empty()) {
     return tl::make_unexpected(url_parse_errc::invalid_port);
   }
@@ -180,38 +180,31 @@ tl::expected<std::uint16_t, url_parse_errc> port_number(std::string_view port) n
   return static_cast<std::uint16_t>(port_value);
 }
 
-bool is_url_code_point(char byte) noexcept {
+auto is_url_code_point(char byte) noexcept {
   return
       std::isalnum(byte, std::locale::classic()) || contains(byte, "!$&'()*+,-./:;=?@_~"sv);
 }
 
-bool is_windows_drive_letter(
-    std::string_view::const_iterator it,
-    std::string_view::const_iterator last) noexcept {
-  if (std::distance(it, last) < 2) {
+inline auto is_windows_drive_letter(std::string_view segment) noexcept {
+  if (segment.size() < 2) {
     return false;
   }
 
-  if (!std::isalpha(*it, std::locale::classic())) {
+  if (!std::isalpha(segment[0], std::locale::classic())) {
     return false;
   }
 
-  ++it;
-  auto result = ((*it == ':') || (*it == '|'));
+  auto result = ((segment[1] == ':') || (segment[1] == '|'));
   if (result) {
-    ++it;
-    if (it != last) {
-      result = ((*it == '/') || (*it == '\\') || (*it == '?') || (*it == '#'));
+    if (segment.size() > 2) {
+      result = ((segment[2] == '/') || (segment[2] == '\\') || (segment[2] == '?') || (segment[2] == '#'));
     }
   }
   return result;
+
 }
 
-inline bool is_windows_drive_letter(std::string_view segment) noexcept {
-  return is_windows_drive_letter(begin(segment), end(segment));
-}
-
-bool is_single_dot_path_segment(std::string_view segment) noexcept {
+inline auto is_single_dot_path_segment(std::string_view segment) noexcept {
   auto lower = std::string(segment);
   std::transform(begin(lower), end(lower), begin(lower),
                  [] (auto byte) -> decltype(byte) {
@@ -221,7 +214,7 @@ bool is_single_dot_path_segment(std::string_view segment) noexcept {
   return ((lower == ".") || (lower == "%2e"));
 }
 
-bool is_double_dot_path_segment(std::string_view segment) noexcept {
+auto is_double_dot_path_segment(std::string_view segment) noexcept {
   auto lower = std::string(segment);
   std::transform(begin(lower), end(lower), begin(lower),
                  [] (auto byte) -> decltype(byte) {
@@ -272,7 +265,7 @@ url_parser_context::url_parser_context(
   it = begin(view);
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_scheme_start(char byte) {
+auto url_parser_context::parse_scheme_start(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (std::isalpha(byte, std::locale::classic())) {
     auto lower = std::tolower(byte, std::locale::classic());
     buffer.push_back(lower);
@@ -289,7 +282,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_scheme_
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_scheme(char byte) {
+auto url_parser_context::parse_scheme(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (std::isalnum(byte, std::locale::classic()) || contains(byte, "+-."sv)) {
     auto lower = std::tolower(byte, std::locale::classic());
     buffer.push_back(lower);
@@ -352,7 +345,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_scheme(
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_no_scheme(char byte) {
+auto url_parser_context::parse_no_scheme(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (!base || (base.value().cannot_be_a_base_url && (byte != '#'))) {
     url.validation_error = true;
     return tl::make_unexpected(url_parse_errc::not_an_absolute_url_with_fragment);
@@ -376,7 +369,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_no_sche
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_special_relative_or_authority(char byte) {
+auto url_parser_context::parse_special_relative_or_authority(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if ((byte == '/') && remaining_starts_with(view.substr(std::distance(std::begin(view), it)), "/"sv)) {
     increment();
     state = url_parse_state::special_authority_ignore_slashes;
@@ -388,7 +381,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_special
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_path_or_authority(char byte) {
+auto url_parser_context::parse_path_or_authority(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (byte == '/') {
     state = url_parse_state::authority;
   } else {
@@ -398,7 +391,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_path_or
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_relative(char byte) {
+auto url_parser_context::parse_relative(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   url.scheme = base.value().scheme;
   if (is_eof()) {
     url.username = base.value().username;
@@ -453,7 +446,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_relativ
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_relative_slash(char byte) {
+auto url_parser_context::parse_relative_slash(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (url.is_special() && ((byte == '/') || (byte == '\\'))) {
     if (byte == '\\') {
       url.validation_error = true;
@@ -475,7 +468,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_relativ
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_special_authority_slashes(char byte) {
+auto url_parser_context::parse_special_authority_slashes(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if ((byte == '/') && remaining_starts_with(view.substr(std::distance(std::begin(view), it)), "/"sv)) {
     increment();
     state = url_parse_state::special_authority_ignore_slashes;
@@ -488,7 +481,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_special
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_special_authority_ignore_slashes(char byte) {
+auto url_parser_context::parse_special_authority_ignore_slashes(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if ((byte != '/') && (byte != '\\')) {
     decrement();
     state = url_parse_state::authority;
@@ -498,7 +491,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_special
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_authority(char byte) {
+auto url_parser_context::parse_authority(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (byte == '@') {
     url.validation_error = true;
     if (at_flag) {
@@ -538,7 +531,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_authori
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_hostname(char byte) {
+auto url_parser_context::parse_hostname(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (state_override && (url.scheme == "file")) {
     state = url_parse_state::file_host;
     if (it == begin(view)) {
@@ -603,7 +596,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_hostnam
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_port(char byte) {
+auto url_parser_context::parse_port(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (std::isdigit(byte, std::locale::classic())) {
     buffer += byte;
   } else if (
@@ -641,7 +634,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_port(ch
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file(char byte) {
+auto url_parser_context::parse_file(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   url.scheme = "file";
 
   if ((byte == '/') || (byte == '\\')) {
@@ -667,7 +660,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file(ch
       url.fragment = std::string();
       state = url_parse_state::fragment;
     } else {
-      if (!is_windows_drive_letter(it, end(view))) {
+      if (!is_windows_drive_letter(view.substr(0, std::distance(begin(view), it)))) {
         url.host = base.value().host;
         url.path = base.value().path;
         shorten_path(url.scheme, url.path);
@@ -692,7 +685,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file(ch
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file_slash(char byte) {
+auto url_parser_context::parse_file_slash(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if ((byte == '/') || (byte == '\\')) {
     if (byte == '\\') {
       url.validation_error = true;
@@ -700,7 +693,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file_sl
     state = url_parse_state::file_host;
   } else {
     if (base &&
-            ((base.value().scheme == "file") && !is_windows_drive_letter(it, end(view)))) {
+            ((base.value().scheme == "file") && !is_windows_drive_letter(view.substr(0, std::distance(begin(view), it))))) {
       if (!base.value().path.empty() && is_windows_drive_letter(base.value().path[0])) {
         url.path.push_back(base.value().path[0]);
       } else {
@@ -715,7 +708,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file_sl
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file_host(char byte) {
+auto url_parser_context::parse_file_host(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if ((is_eof()) || (byte == '/') || (byte == '\\') || (byte == '?') || (byte == '#')) {
     bool at_begin = (it == begin(view));
     if (!at_begin) {
@@ -759,7 +752,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_file_ho
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_path_start(char byte) {
+auto url_parser_context::parse_path_start(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   bool at_begin = (it == begin(view));
   if (url.is_special()) {
     if (byte == '\\') {
@@ -790,7 +783,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_path_st
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_path(char byte) {
+auto url_parser_context::parse_path(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (((is_eof()) || (byte == '/')) ||
       (url.is_special() && (byte == '\\')) ||
       (!state_override && ((byte == '?') || (byte == '#')))) {
@@ -852,7 +845,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_path(ch
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_cannot_be_a_base_url(char byte) {
+auto url_parser_context::parse_cannot_be_a_base_url(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (byte == '?') {
     url.query = std::string();
     state = url_parse_state::query;
@@ -875,7 +868,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_cannot_
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_query(char byte) {
+auto url_parser_context::parse_query(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (!state_override && (byte == '#')) {
     url.fragment = std::string();
     state = url_parse_state::fragment;
@@ -893,7 +886,7 @@ tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_query(c
   return url_parse_action::increment;
 }
 
-tl::expected<url_parse_action, url_parse_errc> url_parser_context::parse_fragment(char byte) {
+auto url_parser_context::parse_fragment(char byte) -> tl::expected<url_parse_action, url_parse_errc> {
   if (byte == '\0') {
     url.validation_error = true;
   } else {

include/skyr/core/url_error.hpp
@@ -44,7 +44,7 @@ enum class url_parse_errc {
 /// Creates a `std::error_code` given a `skyr::url_parse_errc` value
 /// \param error A URL parse error
 /// \returns A `std::error_code` object
-std::error_code make_error_code(url_parse_errc error) noexcept;
+auto make_error_code(url_parse_errc error) noexcept -> std::error_code;
 }  // namespace v1
 }  // namespace skyr
 

include/skyr/core/url_parse.hpp
@@ -18,9 +18,9 @@ inline namespace v1 {
 /// \param input The input string
 /// \param base An optional base URL
 /// \returns A `url_record` on success and an error code on failure
-tl::expected<url_record, std::error_code> parse(
+auto parse(
     std::string_view input,
-    std::optional<url_record> base = std::nullopt);
+    std::optional<url_record> base = std::nullopt) -> tl::expected<url_record, std::error_code>;
 }  // namespace v1
 }  // namespace skyr
 

include/skyr/core/url_record.hpp
@@ -65,17 +65,17 @@ class url_record {
   /// Tests if the URL is a special scheme
   /// \returns `true` if the URL scheme is a special scheme, `false`
   ///          otherwise
-  [[nodiscard]] bool is_special() const noexcept;
+  [[nodiscard]] auto is_special() const noexcept -> bool;
 
   /// Tests if the URL includes credentials
   /// \returns `true` if the URL username or password is not an
   ///          empty string, `false` otherwise
-  [[nodiscard]] bool includes_credentials() const noexcept;
+  [[nodiscard]] auto includes_credentials() const noexcept -> bool;
 
   /// Tests if the URL cannot have a username, password or port
   /// \returns `true` if the URL cannot have a username, password
   ///          or port
-  [[nodiscard]] bool cannot_have_a_username_password_or_port() const noexcept;
+  [[nodiscard]] auto cannot_have_a_username_password_or_port() const noexcept -> bool;
 
   /// Swaps two `url_record` objects
   /// \param other Another `url_record` object

include/skyr/core/url_serialize.hpp
@@ -17,8 +17,8 @@ inline namespace v1 {
 /// \param exclude_fragment A flag to exclude the fragment from
 ///        serialization, if set
 /// \returns A serialized URL string
-url_record::string_type serialize(
-    const url_record &url, bool exclude_fragment = false);
+auto serialize(
+    const url_record &url, bool exclude_fragment = false) -> url_record::string_type;
 }  // namespace v1
 }  // namespace skyr
 

include/skyr/filesystem/path.hpp
@@ -50,18 +50,18 @@ enum class path_errc {
 /// Creates a `std::error_code` given a `skyr::path_errc` value
 /// \param error A filesystem path conversion error
 /// \returns A `std::error_code` object
-std::error_code make_error_code(path_errc error) noexcept;
+auto make_error_code(path_errc error) noexcept -> std::error_code;
 
 /// Converts a path object to a URL with a file protocol. Handles
 /// some processing, including percent encoding
 /// \param path A filesystem path
 /// \returns a url object or an error on failure
-tl::expected<url, std::error_code> from_path(const stdfs::path &path);
+auto from_path(const stdfs::path &path) -> tl::expected<url, std::error_code>;
 
 /// Converts a URL pathname to a filesystem path
 /// \param input A url object
 /// \returns a path object or an error on failure
-tl::expected<stdfs::path, std::error_code> to_path(const url &input);
+auto to_path(const url &input) -> tl::expected<stdfs::path, std::error_code>;
 }  // namespace filesystem
 }  // namespace v1
 }  // namespace skyr

include/skyr/network/ipv4_address.hpp
@@ -12,7 +12,7 @@
 #include <system_error>
 #include <optional>
 #include <tl/expected.hpp>
-#include <skyr/details/endianness.hpp>
+#include <skyr/platform/endianness.hpp>
 
 namespace skyr {
 inline namespace v1 {
@@ -32,7 +32,7 @@ enum class ipv4_address_errc {
 /// value
 /// \param error An IPv4 address error
 /// \returns A `std::error_code` object
-std::error_code make_error_code(ipv4_address_errc error);
+auto make_error_code(ipv4_address_errc error) -> std::error_code;
 
 /// Represents an IPv4 address
 class ipv4_address {
@@ -51,13 +51,13 @@ class ipv4_address {
 
   /// The address value
   /// \returns The address value
-  [[nodiscard]] unsigned int address() const noexcept {
+  [[nodiscard]] auto address() const noexcept {
     return details::from_network_byte_order(address_);
   }
 
   /// The address in bytes in network byte order
   /// \returns The address in bytes
-  [[nodiscard]] std::array<unsigned char, 4> to_bytes() const noexcept {
+  [[nodiscard]] auto to_bytes() const noexcept -> std::array<unsigned char, 4> {
     return {{
       static_cast<unsigned char>(address_ >> 24u),
       static_cast<unsigned char>(address_ >> 16u),
@@ -67,15 +67,15 @@ class ipv4_address {
   }
 
   /// \returns The address as a string
-  [[nodiscard]] std::string to_string() const;
+  [[nodiscard]] auto to_string() const -> std::string;
 
 };
 
 /// Parses an IPv4 address
 /// \param input An input string
 /// \returns An `ipv4_address` object or an error
-tl::expected<ipv4_address, std::error_code> parse_ipv4_address(
-    std::string_view input);
+auto parse_ipv4_address(
+    std::string_view input) -> tl::expected<ipv4_address, std::error_code>;
 }  // namespace v1
 }  // namespace skyr
 

include/skyr/network/ipv6_address.hpp
@@ -13,7 +13,7 @@
 #include <iterator>
 #include <system_error>
 #include <tl/expected.hpp>
-#include <skyr/details/endianness.hpp>
+#include <skyr/platform/endianness.hpp>
 
 namespace skyr {
 inline namespace v1 {
@@ -36,7 +36,7 @@ enum class ipv6_address_errc {
 /// value
 /// \param error An IPv6 address error
 /// \returns A `std::error_code` object
-std::error_code make_error_code(ipv6_address_errc error);
+auto make_error_code(ipv6_address_errc error) -> std::error_code;
 
 /// Represents an IPv6 address
 class ipv6_address {
@@ -58,7 +58,7 @@ class ipv6_address {
 
   /// The address in bytes in network byte order
   /// \returns The address in bytes
-  [[nodiscard]] std::array<unsigned char, 16> to_bytes() const noexcept {
+  [[nodiscard]] auto to_bytes() const noexcept -> std::array<unsigned char, 16> {
     std::array<unsigned char, 16> bytes{};
     for (auto i = 0UL; i < address_.size(); ++i) {
       bytes[i * 2    ] = static_cast<unsigned char>(address_[i] >> 8u);
@@ -68,7 +68,7 @@ class ipv6_address {
   }
 
    /// \returns The IPv6 address as a string
-  [[nodiscard]] std::string to_string() const;
+  [[nodiscard]] auto to_string() const -> std::string;
 
 };
 

commit 9036cf6427dc6f493fddcd143549d8df3dc3e44b

Fixed regression caused in e0c32ad

src/core/url_parser_context.cpp
@@ -201,7 +201,6 @@ inline auto is_windows_drive_letter(std::string_view segment) noexcept {
     }
   }
   return result;
-
 }
 
 inline auto is_single_dot_path_segment(std::string_view segment) noexcept {
@@ -661,7 +660,7 @@ auto url_parser_context::parse_file(char byte) -> tl::expected<url_parse_action,
       url.fragment = std::string();
       state = url_parse_state::fragment;
     } else {
-      if (!is_windows_drive_letter(view.substr(0, std::distance(begin(view), it)))) {
+      if (!is_windows_drive_letter(view.substr(std::distance(begin(view), it)))) {
         url.host = base->host;
         url.path = base->path;
         shorten_path(url.scheme, url.path);
@@ -694,7 +693,7 @@ auto url_parser_context::parse_file_slash(char byte) -> tl::expected<url_parse_a
     state = url_parse_state::file_host;
   } else {
     if (base &&
-            ((base->scheme == "file") && !is_windows_drive_letter(view.substr(0, std::distance(begin(view), it))))) {
+            ((base->scheme == "file") && !is_windows_drive_letter(view.substr(std::distance(begin(view), it))))) {
       if (!base->path.empty() && is_windows_drive_letter(base->path[0])) {
         url.path.push_back(base->path[0]);
       } else {

tests/url/url_tests.cpp
@@ -665,4 +665,11 @@ TEST_CASE("url_tests", "[url]") {
     CHECK(instance.is_domain());
     CHECK(instance.domain().value() == "उदाहरण.परीक्षा");
   }
+
+  SECTION("regression_failure_01") {
+    auto base = skyr::url("file:///tmp/mock/path");
+    auto instance = skyr::url("file:c:\\foo\\bar.html", base);
+    CHECK(instance.protocol() == "file:");
+    CHECK(instance.pathname() == "/c:/foo/bar.html");
+  }
 }