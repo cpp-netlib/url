3c02ad4f4241de7021e75ee3d0e9b212afc9c801
Glyn Matthews <glyn.matthews@gmail.com>
2020-07-09 18:48:09 +0200

    Reworked domain processing to later allow parameterizing allocators

--- include/skyr/v1/domain/domain.hpp
+++ include/skyr/v1/domain/domain.hpp
@@ -55,8 +55,7 @@ inline auto validate_label(std::u32string_view label, [[maybe_unused]] bool use_
     if (it != last) {
       return tl::make_unexpected(domain_errc::bad_input);
     }
-  }
-  else {
+  } else {
     static constexpr auto is_valid_or_deviation = [](auto cp) {
       auto status = idna::code_point_status(cp);
       return (cp <= U'\x7e') || (status == idna::idna_status::valid) || (status == idna::idna_status::deviation);
@@ -71,105 +70,177 @@ inline auto validate_label(std::u32string_view label, [[maybe_unused]] bool use_
   return {};
 }
 
-inline auto domain_to_ascii(
-    std::string_view domain_name, std::string *ascii_domain, bool check_hyphens, bool check_bidi,
-    bool check_joiners, bool use_std3_ascii_rules, bool transitional_processing,
-    bool verify_dns_length) -> tl::expected<void, domain_errc> {
-  /// https://www.unicode.org/reports/tr46/#ToASCII
+///
+struct domain_to_ascii_context {
+  /// Stores the domain as UTF-32
+  std::u32string domain_name;
 
-  using namespace std::string_view_literals;
+  /// Parameters
+  std::string *ascii_domain;
+  bool check_hyphens;
+  bool check_bidi;
+  bool check_joiners;
+  bool use_std3_ascii_rules;
+  bool transitional_processing;
+  bool verify_dns_length;
 
-  auto mapped_domain_name =
-      unicode::as<std::u32string>(unicode::views::as_u8(domain_name) | unicode::transforms::to_u32);
-  if (mapped_domain_name) {
-    auto result = idna::map_code_points(std::begin(mapped_domain_name.value()), std::end(mapped_domain_name.value()),
-                                        use_std3_ascii_rules, transitional_processing);
-    if (result) {
-      mapped_domain_name.value().erase(result.value(), mapped_domain_name.value().cend());
-    } else {
-      return tl::make_unexpected(result.error());
-    }
+  // These are intermediate buffers
+  std::vector<std::u32string> labels;
+  std::string punycode_encoded;
+  std::u32string punycode_decoded;
+};
+
+///
+/// \param domain_name
+/// \param ascii_domain
+/// \param check_hyphens
+/// \param check_bidi
+/// \param check_joiners
+/// \param use_std3_ascii_rules
+/// \param transitional_processing
+/// \param verify_dns_length
+/// \return
+inline auto create_domain_to_ascii_context(std::string_view domain_name, std::string *ascii_domain, bool check_hyphens,
+                                           bool check_bidi, bool check_joiners, bool use_std3_ascii_rules,
+                                           bool transitional_processing, bool verify_dns_length)
+    -> tl::expected<domain_to_ascii_context, domain_errc> {
+  auto u32domain_name = unicode::as<std::u32string>(unicode::views::as_u8(domain_name) | unicode::transforms::to_u32);
+  if (u32domain_name) {
+    return domain_to_ascii_context{u32domain_name.value(), ascii_domain, check_hyphens, check_bidi, check_joiners,
+                                   use_std3_ascii_rules, transitional_processing, verify_dns_length, {}, {}, {}};
   } else {
     return tl::make_unexpected(domain_errc::encoding_error);
   }
-
-  static constexpr auto to_string_view = [] (auto &&label) {
-    return std::u32string_view(std::addressof(*std::cbegin(label)), ranges::distance(label));
-  };
-
-  auto labels = std::vector<std::u32string>{};
-  for (auto &&label : mapped_domain_name.value() | ranges::views::split(U'.') | ranges::views::transform(to_string_view)) {
-    if (labels.size() == labels.max_size()) {
-      return tl::make_unexpected(domain_errc::too_many_labels);
-    }
-
-    if ((label.size() >= 4) && (label.substr(0, 4) == U"xn--")) {
-      auto decoded_label = std::u32string{};
-      auto decoded = punycode_decode(label.substr(4), &decoded_label);
-      if (!decoded) {
-        return tl::make_unexpected(decoded.error());
-      }
-
-      auto validated =
-          validate_label(decoded_label, use_std3_ascii_rules, check_hyphens, check_bidi, check_joiners, false);
-      if (!validated) {
-        return tl::make_unexpected(validated.error());
-      }
-    } else {
-      auto validated = validate_label(label, use_std3_ascii_rules, check_hyphens, check_bidi, check_joiners,
-                                      transitional_processing);
-      if (!validated) {
-        return tl::make_unexpected(validated.error());
-      }
-    }
-
-    constexpr static auto is_ascii = [] (std::u32string_view input) noexcept {
-      constexpr static auto is_in_ascii_set = [](auto c) { return static_cast<unsigned>(c) <= 0x7eu; };
-
-      auto first = std::cbegin(input), last = std::cend(input);
-      return last == std::find_if_not(first, last, is_in_ascii_set);
-    };
-
-    labels.emplace_back();
-    if (!is_ascii(label)) {
-      auto encoded = std::string{};
-      auto result = punycode_encode(label, &encoded);
-      if (!result) {
-        return tl::make_unexpected(result.error());
-      }
-      ranges::copy(U"xn--"sv, ranges::back_inserter(labels.back()));
-      ranges::copy(encoded, ranges::back_inserter(labels.back()));
-    }
-    else {
-      ranges::copy(label, ranges::back_inserter(labels.back()));
-    }
-  }
-
-  if (mapped_domain_name.value().back() == U'.') {
-    labels.emplace_back();
-  }
-
-  constexpr auto max_domain_length = 253;
-  constexpr auto max_label_length = 63;
-
-  if (verify_dns_length) {
-    auto length = mapped_domain_name.value().size();
-    if ((length < 1) || (length > max_domain_length)) {
-      return tl::make_unexpected(domain_errc::invalid_length);
-    }
-
-    for (const auto &label : labels) {
-      auto label_length = label.size();
-      if ((label_length < 1) || (label_length > max_label_length)) {
-        return tl::make_unexpected(domain_errc::invalid_length);
-      }
-    }
-  }
-
-  ranges::copy(labels | ranges::views::join('.'), ranges::back_inserter(*ascii_domain));
-  return {};
 }
 
+///
+/// \param context
+/// \return
+inline auto domain_to_ascii_impl(domain_to_ascii_context &&context)
+  -> tl::expected<void, domain_errc> {
+  /// https://www.unicode.org/reports/tr46/#ToASCII
+
+  constexpr static auto map_domain_name = [] (domain_to_ascii_context &&ctx)
+      -> tl::expected<domain_to_ascii_context, domain_errc> {
+    auto result = idna::map_code_points(std::begin(ctx.domain_name), std::end(ctx.domain_name),
+                                        ctx.use_std3_ascii_rules, ctx.transitional_processing);
+    if (result) {
+      ctx.domain_name.erase(result.value(), ctx.domain_name.cend());
+      return std::move(ctx);
+    } else {
+      return tl::make_unexpected(result.error());
+    }
+  };
+
+  constexpr static auto process_labels = [] (auto &&ctx)
+      -> tl::expected<domain_to_ascii_context, domain_errc> {
+    using namespace std::string_view_literals;
+
+    static constexpr auto to_string_view = [] (auto &&label) {
+      return std::u32string_view(std::addressof(*std::cbegin(label)), ranges::distance(label));
+    };
+
+    for (auto &&label : ctx.domain_name | ranges::views::split(U'.') | ranges::views::transform(to_string_view)) {
+      if ((label.size() >= 4) && (label.substr(0, 4) == U"xn--")) {
+        ctx.punycode_decoded.clear();
+        auto decoded = punycode_decode(label.substr(4), &ctx.punycode_decoded);
+        if (!decoded) {
+          return tl::make_unexpected(decoded.error());
+        }
+
+        auto validated = validate_label(ctx.punycode_decoded, ctx.use_std3_ascii_rules, ctx.check_hyphens,
+                                        ctx.check_bidi, ctx.check_joiners, false);
+        if (!validated) {
+          return tl::make_unexpected(validated.error());
+        }
+      } else {
+        auto validated = validate_label(label, ctx.use_std3_ascii_rules, ctx.check_hyphens, ctx.check_bidi,
+                                        ctx.check_joiners, ctx.transitional_processing);
+        if (!validated) {
+          return tl::make_unexpected(validated.error());
+        }
+      }
+
+      constexpr static auto is_ascii = [] (std::u32string_view input) noexcept {
+        constexpr static auto is_in_ascii_set = [](auto c) { return static_cast<unsigned>(c) <= 0x7eu; };
+
+        auto first = std::cbegin(input), last = std::cend(input);
+        return last == std::find_if_not(first, last, is_in_ascii_set);
+      };
+
+      ctx.labels.emplace_back();
+      if (!is_ascii(label)) {
+        ctx.punycode_encoded.clear();
+        auto result = punycode_encode(label, &ctx.punycode_encoded);
+        if (!result) {
+          return tl::make_unexpected(result.error());
+        }
+        ranges::copy(U"xn--"sv, ranges::back_inserter(ctx.labels.back()));
+        ranges::copy(ctx.punycode_encoded, ranges::back_inserter(ctx.labels.back()));
+      } else {
+        ranges::copy(label, ranges::back_inserter(ctx.labels.back()));
+      }
+    }
+
+    if (ctx.domain_name.back() == U'.') {
+      ctx.labels.emplace_back();
+    }
+
+    return std::move(ctx);
+  };
+
+  constexpr static auto check_length = [] (domain_to_ascii_context &&ctx)
+      -> tl::expected<domain_to_ascii_context, domain_errc> {
+    constexpr auto max_domain_length = 253;
+    constexpr auto max_label_length = 63;
+
+    if (ctx.verify_dns_length) {
+      auto length = ctx.domain_name.size();
+      if ((length < 1) || (length > max_domain_length)) {
+        return tl::make_unexpected(domain_errc::invalid_length);
+      }
+
+      for (const auto &label : ctx.labels) {
+        auto label_length = label.size();
+        if ((label_length < 1) || (label_length > max_label_length)) {
+          return tl::make_unexpected(domain_errc::invalid_length);
+        }
+      }
+    }
+
+    return std::move(ctx);
+  };
+
+  constexpr static auto copy_to_output = [] (domain_to_ascii_context &&ctx)
+                                         -> tl::expected<void, domain_errc> {
+    ranges::copy(ctx.labels | ranges::views::join('.'), ranges::back_inserter(*ctx.ascii_domain));
+    return {};
+  };
+
+  return map_domain_name(std::move(context))
+      .and_then(process_labels)
+      .and_then(check_length)
+      .and_then(copy_to_output)
+      ;
+}
+
+///
+/// \param domain_name
+/// \param ascii_domain
+/// \param check_hyphens
+/// \param check_bidi
+/// \param check_joiners
+/// \param use_std3_ascii_rules
+/// \param transitional_processing
+/// \param verify_dns_length
+/// \return
+inline auto domain_to_ascii(std::string_view domain_name, std::string *ascii_domain, bool check_hyphens,
+                            bool check_bidi, bool check_joiners, bool use_std3_ascii_rules,
+                            bool transitional_processing, bool verify_dns_length) -> tl::expected<void, domain_errc> {
+  return create_domain_to_ascii_context(domain_name, ascii_domain, check_hyphens, check_bidi, check_joiners,
+                                        use_std3_ascii_rules, transitional_processing, verify_dns_length)
+      .and_then(domain_to_ascii_impl);
+}
 
 /// Converts a UTF-8 encoded domain to ASCII using
 /// [IDNA processing](https://www.domain.org/reports/tr46/#Processing)
@@ -186,13 +257,11 @@ inline auto domain_to_ascii(
   auto result = domain_to_ascii(domain_name, ascii_domain, false, true, true, be_strict, false, be_strict);
   if (!result) {
     *validation_error |= true;
-    return tl::make_unexpected(result.error());
-  }
-  else if (ascii_domain->empty()) {
+  } else if (ascii_domain->empty()) {
     *validation_error |= true;
     return tl::make_unexpected(domain_errc::empty_string);
   }
-  return {};
+  return result;
 }
 
 /// Converts a UTF-8 encoded domain to ASCII using
@@ -229,6 +298,13 @@ inline auto domain_to_ascii(std::string_view domain_name, std::string *ascii_dom
   return domain_to_ascii(domain_name, ascii_domain, false, &validation_error);
 }
 
+struct domain_to_u8_context {
+  std::vector<std::string> labels;
+
+  /// This is used as an intermediate buffer
+  std::u32string punycode_decoded;
+};
+
 /// Converts a Punycode encoded domain to UTF-8
 ///
 /// \param domain_name A Punycode encoded domain
@@ -238,41 +314,37 @@ inline auto domain_to_u8(
     std::string *u8_domain,
     [[maybe_unused]] bool *validation_error) -> tl::expected<void, domain_errc> {
 
+  auto context = domain_to_u8_context{};
+
   static constexpr auto to_string_view = [] (auto &&label) {
     return std::string_view(std::addressof(*std::begin(label)), ranges::distance(label));
   };
 
-  auto labels = std::vector<std::string>{};
   for (auto &&label : domain_name | ranges::views::split('.') | ranges::views::transform(to_string_view)) {
-    if (labels.size() == labels.max_size()) {
-      return tl::make_unexpected(domain_errc::too_many_labels);
-    }
-
-    labels.emplace_back();
+    context.labels.emplace_back();
     if (label.substr(0, 4) == "xn--") {
       label.remove_prefix(4);
-      auto decoded = std::u32string{};
-      auto result = punycode_decode(label, &decoded);
+      context.punycode_decoded.clear();
+      auto result = punycode_decode(label, &context.punycode_decoded);
       if (!result) {
         return tl::make_unexpected(result.error());
       }
-      auto u8 = decoded | unicode::transforms::to_u8;
+      auto u8 = context.punycode_decoded | unicode::transforms::to_u8;
       auto first = std::cbegin(u8);
       auto last = std::cend(u8);
       for (auto it = first; it != last; ++it) {
-        labels.back().push_back((*it).value());
+        context.labels.back().push_back((*it).value());
       }
-    }
-    else {
-      ranges::copy(label, ranges::back_inserter(labels.back()));
+    } else {
+      ranges::copy(label, ranges::back_inserter(context.labels.back()));
     }
   }
 
   if (domain_name.back() == U'.') {
-    labels.emplace_back();
+    context.labels.emplace_back();
   }
 
-  ranges::copy(labels | ranges::views::join('.'), ranges::back_inserter(*u8_domain));
+  ranges::copy(context.labels | ranges::views::join('.'), ranges::back_inserter(*u8_domain));
   return {};
 }
 
