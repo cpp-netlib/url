31684d8c2e26e1cb8d13370f9c43c8c72be201a8
Glyn Matthews <glyn.matthews@gmail.com>
2020-08-27 11:50:01 +0200

    Replaced unique_ptr in test with a shared_ptr

--- tests/containers/static_vector_tests.cpp
+++ tests/containers/static_vector_tests.cpp
@@ -3,31 +3,35 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-
-#include <string>
+#include <memory>
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 #include <skyr/v1/containers/static_vector.hpp>
 
-struct test_destructor_call {
-  bool *destructed;
 
-  test_destructor_call(bool *destructed)
+struct test_destructor_call {
+  bool *destructed = nullptr;
+
+  explicit test_destructor_call(bool *destructed)
   : destructed(destructed) {
     *destructed = false;
   }
 
+  test_destructor_call(const test_destructor_call &) = delete;
+  test_destructor_call &operator=(const test_destructor_call &) = delete;
+  test_destructor_call(test_destructor_call &&) = delete;
+  test_destructor_call &operator=(test_destructor_call &&) = delete;
+
   ~test_destructor_call() {
     *destructed = true;
   }
-
 };
 
 TEST_CASE("pop back calls destructor", "[containers]") {
   auto destructed = false;
 
-  auto vector = skyr::static_vector<std::unique_ptr<test_destructor_call>, 8>{};
-  vector.emplace_back(std::make_unique<test_destructor_call>(&destructed));
+  auto vector = skyr::static_vector<std::shared_ptr<test_destructor_call>, 8>{};
+  vector.emplace_back(std::make_shared<test_destructor_call>(&destructed));
   CHECK_FALSE(destructed);
   vector.pop_back();
   CHECK(destructed);
@@ -36,8 +40,8 @@ TEST_CASE("pop back calls destructor", "[containers]") {
 TEST_CASE("clear calls destructor", "[containers]") {
   auto destructed = false;
 
-  auto vector = skyr::static_vector<std::unique_ptr<test_destructor_call>, 8>{};
-  vector.emplace_back(std::make_unique<test_destructor_call>(&destructed));
+  auto vector = skyr::static_vector<std::shared_ptr<test_destructor_call>, 8>{};
+  vector.emplace_back(std::make_shared<test_destructor_call>(&destructed));
   CHECK_FALSE(destructed);
   vector.clear();
   CHECK(destructed);
