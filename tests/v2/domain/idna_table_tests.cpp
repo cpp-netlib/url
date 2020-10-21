2a6df2691bb0253333b33e991f216384ccea79ca
Glyn Matthews <glyn.matthews@gmail.com>
2020-10-21 12:36:23 +0200

    Updated IDNA table tests that were not compiling on CI using C++20 compiler

--- tests/v2/domain/idna_table_tests.cpp
+++ tests/v2/domain/idna_table_tests.cpp
@@ -8,18 +8,23 @@
 #include <skyr/v2/domain/idna.hpp>
 
 
+struct parameter {
+  char32_t value;
+  skyr::idna::idna_status status;
+};
+
+
 TEST_CASE("IDNA character values", "[idna]") {
-  using param = std::pair<char32_t, skyr::idna::idna_status>;
 
   auto code_point = GENERATE(
-          param{0x0000, skyr::idna::idna_status::disallowed_std3_valid},
-          param{0x002d, skyr::idna::idna_status::valid},
-          param{0x10fffd, skyr::idna::idna_status::disallowed},
-          param{0x10ffff, skyr::idna::idna_status::disallowed});
+      parameter{0x0000, skyr::idna::idna_status::disallowed_std3_valid},
+      parameter{0x002d, skyr::idna::idna_status::valid},
+      parameter{0x10fffd, skyr::idna::idna_status::disallowed},
+      parameter{0x10ffff, skyr::idna::idna_status::disallowed});
 
   SECTION("code_point_set") {
     const auto [value, status] = code_point;
-    INFO("0x" << std::hex << value << ", " << static_cast<int>(status));
+    INFO("0x" << std::hex << static_cast<std::uint32_t>(value) << ", " << static_cast<int>(status));
     REQUIRE(status == skyr::idna::code_point_status(value));
   }
 }
